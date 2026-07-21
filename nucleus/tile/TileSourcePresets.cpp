3588740af0b6b19615e0ca51a1c4442ecf92d6be
Gerald Kimmersdorfer <gerald.kimmersdorfer@gmail.com>
2026-07-21 11:00:45 +0200

    add: debug snow dataset for 2026-02-01

--- nucleus/tile/TileSourcePresets.cpp
+++ nucleus/tile/TileSourcePresets.cpp
@@ -43,6 +43,8 @@ const std::vector<TileSourcePreset> presets = {
         TileLoadService::UrlPattern::ZYX_yPointingSouth, ".png", 256, 20 },
     { "Debug Localhost Vienna", "debug_localhost_vienna", "http://127.0.0.1:8001/v1/debug-ortho/", TileLoadService::UrlPattern::ZYX_yPointingSouth,
         ".jpeg", 256, 21 },
+    { "Snow", "snow", "http://127.0.0.1:8001/v1/cosmos-snow/2026-02-01/", TileLoadService::UrlPattern::ZXY_yPointingSouth,
+        ".png", 256, 13 },
 };
 } // namespace
 
