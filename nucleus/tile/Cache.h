d1bdc02e36fc85978ab37def4bd3847e1fc53841
Gerald Kimmersdorfer <gerald.kimmersdorfer@gmail.com>
2026-09-23 15:38:41 +0200

    add: update copyrights

--- nucleus/tile/Cache.h
+++ nucleus/tile/Cache.h
@@ -1,6 +1,7 @@
 /*****************************************************************************
  * AlpineMaps.org
  * Copyright (C) 2023 Adam Celarek
+ * Copyright (C) 2026 Gerald Kimmersdorfer
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
